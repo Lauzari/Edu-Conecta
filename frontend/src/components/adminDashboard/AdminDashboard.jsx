844e2a54caf8d5437e8ddd10b4c27713421bb626
Lauzari <mlauramoyano1985@gmail.com>
2025-09-10 12:02:35 -0300

    initial w english

--- frontend/src/components/adminDashboard/AdminDashboard.jsx
+++ frontend/src/components/adminDashboard/AdminDashboard.jsx
@@ -1,19 +1,19 @@
-export default function AdminDashboard({ usuarios, profesores }) {
+export default function AdminDashboard({ users, professors }) {
     return (
         <div className="admin-dashboard">
             <h3>Panel de Administrador</h3>
 
             <h4>Usuarios Registrados:</h4>
             <ul>
-                {usuarios.map((u, idx) => (
+                {users.map((u, idx) => (
                     <li key={idx}>{u.Nombre} - {u.Email}</li>
                 ))}
             </ul>
 
             <h4>Postulaciones de Profesores:</h4>
             <ul>
-                {profesores.map((p, idx) => (
-                    <li key={idx}>{p.NombreProfesor} - {p.Email} - {p.Materia}</li>
+                {professors.map((p, idx) => (
+                    <li key={idx}>{p.NameProfessor} - {p.Email} - {p.Subject}</li>
                 ))}
             </ul>
         </div>
