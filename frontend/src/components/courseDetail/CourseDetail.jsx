a52090ec2aeffc2ab528e5b737a70479978c7a16
Justina-R <justina.rey2602@gmail.com>
2025-11-12 18:50:26 -0300

    Fix css connection in CourseDetail

--- frontend/src/components/courseDetail/CourseDetail.jsx
+++ frontend/src/components/courseDetail/CourseDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./CourseDetail.css";
+import "./courseDetail.css";
 import { useAuth } from "../../hooks/useAuth";
 import { toast } from "react-toastify";
 
